0fde59bf9ad9dacea389b8011423bc0f0f69098a
vladistr08 <vladistra08@gmail.com>
2020-06-24 12:50:28 +0300

    flask login/register

--- flask_mongo/main.py
+++ flask_mongo/main.py
@@ -5,15 +5,37 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb://localhost:27017/moni_time"
 mongo = PyMongo(app)
 
-@app.route('/upload', methods=['POST'])
-def post_json():
+@app.route('/login', methods=['POST'])
+def post_loin():
     req_data = request.get_json()
-    mongo.db.Prox.insert(dict(req_data))
-    return dict(req_data)
+    req_data = dict(req_data)
+
+    user = req_data["username"]
+    password = req_data["password"]
+    
+    x = mongo.db.users.find_one({"username":user,"password":password})
+    return "ok" if x != None else "404"
+
+@app.route('/register', methods=['POST'])
+def post_register():
+    req_data = request.get_json()
+    req_data = dict(req_data)
+
+    email = req_data["email"]
+    username = req_data["username"]
+    password = req_data["password"]
+
+    for x in mongo.db.users.find():
+        if x["email"] == email or x["username"] == username:
+            return "User deja existent!!!"
+
+    x = {"username":username, "email":email, "password":password}
+    mongo.db.users.insert_one(x)
+    return "User-ul a fost adaugat cu succes"
 
 @app.route('/list/<username>')
 def getProcesses(username):
-    user_profile = mongo.db.Prox.find_one({'username':username})
+    user_profile = mongo.db.users.find_one({'username':username})
     return str(user_profile)
 
-app.run(host = '0.0.0.0')
+app.run(host = '0.0.0.0', debug=False)
